6236ef27983c26e118e577716c41386e642a489c
ukushagra8-ui <ukushagra8@gmail.com>
2025-08-28 16:23:33 +0530

    Add files via upload

---
+++ Kushagra C programmes/Countdown.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+
+    int Initial_Countdown ;
+
+    printf("Enter the number from where you start the Countdown = ");
+    scanf("%d",&Initial_Countdown);
+
+    if(Initial_Countdown > 0)
+    {
+
+        while(Initial_Countdown >0)
+        {
+
+            printf("Countdown : %d\n",Initial_Countdown);
+            sleep(1);
+            Initial_Countdown-- ;
+
+        }
+        printf("Countdown Completed.!");
+    }
+    else printf("You have entered negative number which is invalid") ;
+
+
+    return 0;
+
+
+
+}
