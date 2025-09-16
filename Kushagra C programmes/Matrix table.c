cd4c25072d19894a05925820a3276053daaec442
ukushagra8-ui <ukushagra8@gmail.com>
2025-09-16 08:42:24 +0530

    Add files via upload

---
+++ Kushagra C programmes/Matrix table.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+
+
+int col,row,i,j;
+printf("\nEnter Column Numbers = ");
+scanf("%d",&col);
+
+printf("\nEnter Row Numbers = ");
+scanf("%d",&row);
+int arr[i][j];
+printf("\nThe Multiplication Table\n");
+
+for(i=0;i<+row;i++){
+
+for(j=0;j<+col;j++){
+
+printf("%10d",(i+1)*(j+1));
+
+} printf("\n");
+}
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+}
