bd69a173839a279d65b77e1532c8020c6286bd1b
Kushagra VU <ukushagra8@gmail.com>
2025-10-14 11:15:31 +0530

    Librarybooks.c

--- Kushagra C programmes/Librarybooks.c
+++ Kushagra C programmes/Librarybooks.c
@@ -10,11 +10,11 @@ void bookname(int *n, char name[][50], int status[]) {
 }
 
 
-void borrow(int *n, char borrowed[], char name[][50], int status[]) {
-    int found = 0;
+void borrow(int *n, char borrowed[], char name[][50], int status[],int *nfound) {
+   
     for(int i = 0; i < *n; i++) {
         if(strcmp(borrowed, name[i]) == 0) {
-            found = 1;
+            *nfound = 1;
             if(status[i] == 1) {
                 printf("Sorry! '%s' is already borrowed.\n", name[i]);
             } else {
@@ -23,11 +23,8 @@ void borrow(int *n, char borrowed[], char name[][50], int status[]) {
             }
             break;
         }
-    }
-    if(!found) {
-        printf("Book '%s' not found in the library.\n", borrowed);
-    }
-}
+    } }
+    
 
 
 int calculateFine(int lateDays) {
@@ -53,17 +50,21 @@ int main() {
     bookname(&number, name, status);
 
     while(1) {
-        int enter;
+        int enter,found =0;
         printf("\nEnter 0 to exit, or any other number to continue: ");
         scanf("%d", &enter);
         if(enter == 0) break;
+        
 
         printf("Enter the book you want to borrow: ");
         scanf(" %[^\n]", borrowed);
 
-        borrow(&number, borrowed, name, status);
+        borrow(&number, borrowed, name, status,&found);
+        if(found==0){
+            printf("Book %s not found in library, Try again ",borrowed);
+            continue;
+        }
         bookname(&number, name, status);
-
         
         int late;
         printf("Enter number of late days for '%s': ", borrowed);
