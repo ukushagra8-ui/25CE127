52cadac13238361f7c8ffa65ae34641ab99920e4
Kushagra VU <ukushagra8@gmail.com>
2025-11-06 15:05:04 +0530

    Update sorting with pointers.c

--- Kushagra C programmes/sorting with pointers.c
+++ Kushagra C programmes/sorting with pointers.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int items;
+    int flag=0;
     printf("Enter number of items: ");
-    if (scanf("%d", &items) != 1 || items < 0) {
+    if (scanf("%d", &items) != 1 || items < 0)
+    {
         printf("Error: Invalid number of items.\n");
         return 1;
     }
 
-    if (items == 0) {
+    if (items == 0)
+    {
         printf("Error: \"No items to sort.\"\n");
         return 0;
     }
@@ -16,19 +20,28 @@ int main() {
     float price[items];
     float *ptr;
 
-   
-    for (ptr = price; ptr < &price[items]; ptr++) {
+
+    for (ptr = price; ptr < &price[items]; ptr++)
+    {
         printf("Enter price of item %d: ",ptr - price + 1);
-        if (scanf("%f", ptr) != 1) {
-            printf("Error: \"Invalid input for price. Please enter numeric values only.\"\n");
-            return 1;
+        if (scanf("%f", ptr) != 1)
+        {
+            flag=1;
+            fflush(stdin);
         }
     }
+    if(flag==1)
+    {
+        printf("Error");
+        return 1;
+    }
 
-    
-    for (ptr = price; ptr < &price[items - 1]; ptr++) {
-        for (float *j = ptr + 1; j < &price[items]; j++) {
-            if (*ptr > *j) {
+    for (ptr = price; ptr < &price[items - 1]; ptr++)
+    {
+        for (float *j = ptr + 1; j < &price[items]; j++)
+        {
+            if (*ptr > *j)
+            {
                 float temp = *ptr;
                 *ptr = *j;
                 *j = temp;
@@ -36,9 +49,10 @@ int main() {
         }
     }
 
-    
+
     printf("Sorted Prices: ");
-    for (ptr = price; ptr < &price[items]; ptr++) {
+    for (ptr = price; ptr < &price[items]; ptr++)
+    {
         printf("%.2f", *ptr);
         if (ptr < &price[items - 1]) printf(", ");
     }
